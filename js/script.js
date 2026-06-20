f61f9f6621dfdcd090deb33c497209f295ef2934
Steven Sánchez <st478gtx@outlook.com>
2026-06-19 22:45:48 -0500

    Oferta tabla completa.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,129 @@
 let selectedIndex = 0;
 
 const primaria = [
-  { curso: "Matemática", hora: "8 horas", area: "Ciencias" },
-  { curso: "Comunicación", hora: "5 horas", area: "Letras" },
-  { curso: "Inglés", hora: "4 horas", area: "Idiomas" },
-  { curso: "Educación Fisica", hora: "2 horas", area: "Deportes" },
-  { curso: "Arte y Cultura", hora: "2 horas", area: "Arte" },
-  { curso: "Computación", hora: "2 horas", area: "Tecnología" },
+  {
+    curso: "Matemática",
+    hora: "8 horas",
+    area: "Ciencias",
+    tipo: "Obligatorio",
+  },
+  {
+    curso: "Comunicación",
+    hora: "5 horas",
+    area: "Letras",
+    tipo: "Obligatorio",
+  },
+  { curso: "Inglés", hora: "4 horas", area: "Idiomas", tipo: "Obligatorio" },
+  {
+    curso: "Educación Fisica",
+    hora: "2 horas",
+    area: "Deportes",
+    tipo: "Regular",
+  },
+  {
+    curso: "Arte y Cultura",
+    hora: "2 horas",
+    area: "Arte",
+    tipo: "Regular",
+  },
+  {
+    curso: "Computación",
+    hora: "2 horas",
+    area: "Tecnología",
+    tipo: "Taller",
+  },
 ];
 
 const secundaria1 = [
-  { curso: "Matemática", hora: "6 horas", area: "Ciencias" },
-  { curso: "Comunicación", hora: "5 horas", area: "Letras" },
-  { curso: "Ciencias Naturales", hora: "4 horas", area: "Ciencias" },
-  { curso: "Historia y Geografia", hora: "4 horas", area: "Letras" },
-  { curso: "Inglés", hora: "4 horas", area: "Idiomas" },
-  { curso: "Educación Fisica", hora: "2 horas", area: "Deportes" },
-  { curso: "Arte y Cultura", hora: "2 horas", area: "Arte" },
-  { curso: "Computación", hora: "2 horas", area: "Tecnología" },
+  {
+    curso: "Matemática",
+    hora: "6 horas",
+    area: "Ciencias",
+    tipo: "Obligatorio",
+  },
+  {
+    curso: "Comunicación",
+    hora: "5 horas",
+    area: "Letras",
+    tipo: "Obligatorio",
+  },
+  {
+    curso: "Ciencias Naturales",
+    hora: "4 horas",
+    area: "Ciencias",
+    tipo: "Obligatorio",
+  },
+  {
+    curso: "Historia y Geografia",
+    hora: "4 horas",
+    area: "Letras",
+    tipo: "Obligatorio",
+  },
+  { curso: "Inglés", hora: "4 horas", area: "Idiomas", tipo: "Obligatorio" },
+  {
+    curso: "Educación Fisica",
+    hora: "2 horas",
+    area: "Deportes",
+    tipo: "Regular",
+  },
+  {
+    curso: "Arte y Cultura",
+    hora: "2 horas",
+    area: "Arte",
+    tipo: "Regular",
+  },
+  {
+    curso: "Computación",
+    hora: "2 horas",
+    area: "Tecnología",
+    tipo: "Taller",
+  },
 ];
 
 const secundaria2 = [
-  { curso: "Matemática", hora: "8 horas", area: "Ciencias" },
-  { curso: "Comunicación", hora: "5 horas", area: "Letras" },
-  { curso: "Ciencias Naturales", hora: "5 horas", area: "Ciencias" },
-  { curso: "Historia y Geografia", hora: "5 horas", area: "Letras" },
-  { curso: "Inglés", hora: "4 horas", area: "Idiomas" },
-  { curso: "Educación Fisica", hora: "4 horas", area: "Deportes" },
-  { curso: "Arte y Cultura", hora: "3 horas", area: "Arte" },
-  { curso: "Computación", hora: "5 horas", area: "Tecnología" },
+  {
+    curso: "Matemática",
+    hora: "8 horas",
+    area: "Ciencias",
+    tipo: "Obligatorio",
+  },
+  {
+    curso: "Comunicación",
+    hora: "5 horas",
+    area: "Letras",
+    tipo: "Obligatorio",
+  },
+  {
+    curso: "Ciencias Naturales",
+    hora: "5 horas",
+    area: "Ciencias",
+    tipo: "Obligatorio",
+  },
+  {
+    curso: "Historia y Geografia",
+    hora: "5 horas",
+    area: "Letras",
+    tipo: "Obligatorio",
+  },
+  { curso: "Inglés", hora: "4 horas", area: "Idiomas", tipo: "Obligatorio" },
+  {
+    curso: "Educación Fisica",
+    hora: "4 horas",
+    area: "Deportes",
+    tipo: "Regular",
+  },
+  {
+    curso: "Arte y Cultura",
+    hora: "3 horas",
+    area: "Arte",
+    tipo: "Regular",
+  },
+  {
+    curso: "Computación",
+    hora: "4 horas",
+    area: "Tecnología",
+    tipo: "Taller",
+  },
 ];
 
 //Agregar mas curricula escolar secundaria 3, 4 y 5
@@ -48,6 +143,7 @@ let agregarDatos = (array) => {
 
   array.forEach((element) => {
     const nuevaFila = document.createElement("tr");
+    nuevaFila.classList = "tabla-font-size";
 
     const curso = document.createElement("td");
     curso.textContent = element.curso;
@@ -58,8 +154,16 @@ let agregarDatos = (array) => {
     nuevaFila.appendChild(hora);
 
     const area = document.createElement("td");
-    area.textContent = element.hora;
+    area.textContent = element.area;
     nuevaFila.appendChild(area);
+
+    const tipo = document.createElement("td");
+    const tipoStyle = document.createElement("span");
+    tipo.className = "text-center"
+    tipoStyle.textContent = element.tipo;
+    tipoStyle.classList = selecionTipo(element.tipo);
+    tipo.appendChild(tipoStyle);
+    nuevaFila.appendChild(tipo);
 
     container.appendChild(nuevaFila);
   });
@@ -69,12 +173,12 @@ const container = document.getElementById("option-oferta");
 
 container.addEventListener("click", (event) => {
   if (event.target.tagName === "A") {
-    let id = event.target.id;    
+    let id = event.target.id;
 
     container
       .querySelectorAll(".nav-link")
-      .forEach((b) => b.classList.remove("active"));
-    event.target.classList.add("active");
+      .forEach((b) => b.classList.remove("opcion-activa"));
+    event.target.classList.add("opcion-activa");
 
     agregarDatos(lista[id].curricula);
   }
@@ -86,18 +190,39 @@ let agregarLista = (array) => {
     option.className = "nav-item";
     const text = document.createElement("a");
     text.className = "nav-link";
-    text.href = "#";
     text.textContent = item.grado;
     text.id = index;
 
     if (selectedIndex == index) {
-      text.classList.toggle("active");
+      text.classList.toggle("opcion-activa");
     }
 
     option.appendChild(text);
 
     container.appendChild(option);
   });
+};
+
+let selecionTipo = (tipo) => {
+  let tipoClass;
+  switch (tipo) {
+    case "Obligatorio":
+      tipoClass = "tipo-obligatorio";
+      break;
+
+    case "Regular":
+      tipoClass = "tipo-regular";
+      break;
+
+    case "Taller":
+      tipoClass = "tipo-taller";
+      break;
+
+    default:
+      tipoClass = "none";
+      break;
+  }
+  return tipoClass;
 };
 
 function seleccionar(id) {
