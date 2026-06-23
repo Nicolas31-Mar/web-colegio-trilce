083b41124ce11efde9c1a02fcfcc8cbf99de715c
Steven Sánchez <st478gtx@outlook.com>
2026-06-23 03:51:26 -0500

    Modal a matricula.

---
+++ js/matricula.js
@@ -0,0 +1,71 @@
+let nombre = document.getElementById("nombre");
+let documento = document.getElementById("documento");
+let email = document.getElementById("email");
+let grado = document.getElementById("grado");
+let sede = document.getElementById("sede");
+
+let formulario = document.getElementById("formulario-matricula");
+let matricuale = document.getElementById("matriculate");
+
+const myModal = document.getElementById("modal");
+
+formulario.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+
+  if (!formulario.checkValidity()) {
+    return;
+  }
+});
+
+let n = "";
+let d = "";
+let e = "";
+let g = "";
+let s = "";
+
+function validarCampos() {
+  return (
+    isNotEmptyString(n) &&
+    isNotEmptyString(d) &&
+    isNotEmptyString(e) &&
+    isNotEmptyString(g) &&
+    isNotEmptyString(s)
+  );
+}
+
+nombre.addEventListener("change", (event) => {
+  n = event.target.value;
+});
+
+documento.addEventListener("change", (event) => {
+  d = event.target.value;
+});
+
+email.addEventListener("change", (event) => {
+  e = event.target.value;
+});
+
+grado.addEventListener("change", (event) => {
+  g = event.target.value;
+});
+
+sede.addEventListener("change", (event) => {
+  s = event.target.value;
+});
+
+formulario.addEventListener("change", (event) => {
+  if (event.target.value === "INPUT" || "SELECT") {
+    let valor = event.target.value;
+
+    if (validarCampos()) {
+      matricuale.setAttribute("data-bs-toggle", "modal");
+      matricuale.setAttribute("data-bs-target", "#modal");
+    } else {
+      matricuale.removeAttribute("data-bs-toggle");
+    }
+  }
+});
+
+function isNotEmptyString(value) {
+  return typeof value !== "string" || value.trim().length !== 0;
+}
